8c0775600fab215ab4f70f55fc12f55485214aa3
mollykyle <mekyle@loyola.edu>
2024-02-08 16:23:12 -0500

    started code

--- marioKart.java
+++ marioKart.java
@@ -1,12 +1,30 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
-
-class HelloWorld {
-    public static void main(String[] args) {
+//Programmers: Molly Kyle
+//Course: CS 212 Mr. Nweke
+//Due Date: 2/8/24
+//Lab Assignment: Class Assignment
+//Problem Statement:
+//Input:
+//Output:
+//Credits:
+public class MarioKartPlayer { //Start of class
+   // public static void main(String[] args) {
 
         System.out.println("Mario Kart!");
-        
+    //Data fields
+    private String playerName;
+    private String character;
+    private int speed;
+
+    //General Constructor (parameterized)
+
+    public MarioKartPlayer(String playerName, String character, int speed){
+        this.playerName = playerName;
+        this.character = character;
+        this.speed = speed;
+    }
+
+    //Default Constructor with default values
+    public MarioKartPlayer() {
+        this.playerName = "Mario";
     }
 }
